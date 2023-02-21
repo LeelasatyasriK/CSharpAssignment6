036caf4705432d9ed7dde3f693ac5f641eab198e
LeelasatyasriK <121661802+LeelasatyasriK@users.noreply.github.com>
2023-02-21 17:05:50 +0530

    Add files via upload

---
+++ DelegatesExample/DelegatesProgram.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace UnicastDelegate
+{
+    public delegate void dele();
+        class DelegatesProgram
+        {
+            static void Main(string[] args)
+            {
+                MarketingExecutive m = new MarketingExecutive();
+                Console.WriteLine("Enter Employee Number: ");
+                m.EmpNo = int.Parse(Console.ReadLine());
+
+                Console.Write("Enter Employee Name: ");
+                m.EmpName = Console.ReadLine();
+
+                Console.Write("Enter Employee salary: ");
+                m.Salary = int.Parse(Console.ReadLine());
+
+
+                Manager m1 = new Manager();
+
+               Console.WriteLine("Enter Employee Number: ");
+               m1.EmpNo = int.Parse(Console.ReadLine());
+
+              Console.Write("Enter Employee Name: ");
+              m1.EmpName = Console.ReadLine();
+
+               Console.Write("Enter Employee salary: ");
+               m1.Salary = int.Parse(Console.ReadLine());
+               dele del = new dele(m1.PrintDetails);
+                 del += new dele(m.PrintDetails);
+           
+                Console.WriteLine("Enter Kilometers travelled");
+                int n = Convert.ToInt32(Console.ReadLine());
+                m.KT = n;
+                m.TeA = 1000;
+                m.CalculateSalary();
+            }
+        }
+}
