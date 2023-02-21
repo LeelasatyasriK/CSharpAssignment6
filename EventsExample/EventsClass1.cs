433e0d3cd5eda6ffef1949b9111b79ed277bc1c7
LeelasatyasriK <121661802+LeelasatyasriK@users.noreply.github.com>
2023-02-21 17:10:33 +0530

    Add files via upload

---
+++ EventsExample/EventsClass1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventsQuestion
+{
+   class c1
+    {
+        public static void zerobalEventHandler()
+        {
+                
+              Console.WriteLine("Account Balance is zero ");
+        }
+        public static void underbalEventHandler()
+        {
+
+            Console.WriteLine("Account Balance is not sufficient ");
+        }
+    }
+
+}
